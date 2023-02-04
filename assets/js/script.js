f3be995925ef2d3f0ef16e105e18cc28fa472c2f
Althias <pajotinm@gmail.com>
2023-02-04 21:57:05 +0100

    use js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,33 @@
-function burgerMenu() {
-  const burger = document.querySelector("#burger-menu");
-  const menu = document.querySelector("#nav-menu");
 
-  burger.addEventListener("click", function() {
-  menu.style.display = menu.style.display === "block" ? "none" : "block";
-  });
-}
+// toggle-nav 
+const toggleBtns = document.querySelectorAll('.toggle-nav');
+toggleBtns.forEach(btn => {
+  btn.addEventListener('click', function () {
+    const targetId = this.dataset.toggle;
+    const sections = document.querySelectorAll('.section');
 
-function navDisplay () {
-  const toggleBtns = document.querySelectorAll('.toggle-nav');
-
-  toggleBtns.forEach(btn => {
-    btn.addEventListener('click', function () {
-      const targetId = this.dataset.toggle;
-      const sections = document.querySelectorAll('.section');
-
-      sections.forEach(section => {
-        if (section.id === targetId) {
-          section.style.display = "block";
-        } else {
-          section.style.display = "none";
-        }
-      });
+    sections.forEach(section => {
+      if (section.id === targetId) {
+        section.style.display = "block";
+      } else {
+        section.style.display = "none";
+      }
     });
   });
-}
+});
+
+// include navigation
+const navigationContainer = document.querySelector("#navigation");
+fetch("/assets/html/navigation.html")
+  .then(response => response.text())
+  .then(headerHTML => {
+    navigationContainer.innerHTML = headerHTML;
+  });
+
+// include banner 
+const bannerContainer = document.querySelector("#banner");
+fetch("/assets/html/banner.html")
+  .then(response => response.text())
+  .then(headerHTML => {
+    bannerContainer.innerHTML = headerHTML;
+  });
